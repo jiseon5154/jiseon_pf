f46eed53c1579f0adbda0307e35fb72f117db352
jiseon5154 <julie8800@naver.com>
2021-09-13 18:34:18 +0900

    추가 수정

--- docs/aboutMe.js
+++ docs/aboutMe.js
@@ -1,3 +1,53 @@
+function Popup__isActived(no) {
+  return $(`.popup-${no}`).hasClass(`active`);
+}
+
+function Popup__toggle(no) {
+  if ( Popup__isActived(no) ) {
+    Popup__hide(no);
+  }
+  else {
+    Popup__show(no);
+  }
+}
+
+function Popup__show(no) {
+  $(`.popup-${no}`).addClass(`active`);
+  $(`.btn-show-popup-${no}`).addClass(`active`);
+  $(`.btn-toggle-popup-${no}`).addClass(`active`);
+
+  $(`html`).addClass(`popup-${no}-actived`);
+}
+
+function Popup__hide(no) {
+  $(`.popup-${no}`).removeClass(`active`);
+  $(`.btn-show-popup-${no}`).removeClass(`active`);
+  $(`.btn-toggle-popup-${no}`).removeClass(`active`);
+
+  $(`html`).removeClass(`popup-${no}-actived`);
+}
+
+function Popup__init(no) {
+  $(`.btn-show-popup-${no}`).click(function() {
+    Popup__show(no);
+  });
+
+  $(`.btn-toggle-popup-${no}`).click(function() {
+    Popup__toggle(no);
+  });
+
+  $(`.popup-${no}, .popup-${no} .popup__btn-close`).click(function() {
+    Popup__hide(no);
+  });
+
+  $(`.popup-${no} .popup__content`).click(function() {
+    return false;
+  });
+}
+
+Popup__init(1);
+Popup__init(2);
+
 function Section1__init() {
   const tl = gsap.timeline({
     repeat:-1,
--- docs/experience.js
+++ docs/experience.js
@@ -15,6 +15,56 @@ function BtnGoTop__init() {
 
 BtnGoTop__init();
 
+function Popup__isActived(no) {
+  return $(`.popup-${no}`).hasClass(`active`);
+}
+
+function Popup__toggle(no) {
+  if ( Popup__isActived(no) ) {
+    Popup__hide(no);
+  }
+  else {
+    Popup__show(no);
+  }
+}
+
+function Popup__show(no) {
+  $(`.popup-${no}`).addClass(`active`);
+  $(`.btn-show-popup-${no}`).addClass(`active`);
+  $(`.btn-toggle-popup-${no}`).addClass(`active`);
+
+  $(`html`).addClass(`popup-${no}-actived`);
+}
+
+function Popup__hide(no) {
+  $(`.popup-${no}`).removeClass(`active`);
+  $(`.btn-show-popup-${no}`).removeClass(`active`);
+  $(`.btn-toggle-popup-${no}`).removeClass(`active`);
+
+  $(`html`).removeClass(`popup-${no}-actived`);
+}
+
+function Popup__init(no) {
+  $(`.btn-show-popup-${no}`).click(function() {
+    Popup__show(no);
+  });
+
+  $(`.btn-toggle-popup-${no}`).click(function() {
+    Popup__toggle(no);
+  });
+
+  $(`.popup-${no}, .popup-${no} .popup__btn-close`).click(function() {
+    Popup__hide(no);
+  });
+
+  $(`.popup-${no} .popup__content`).click(function() {
+    return false;
+  });
+}
+
+Popup__init(1);
+Popup__init(2);
+
 function Ab1__init() {
   gsap.from('.ex-2_image1', {
     ease: Expo.easeIn,
